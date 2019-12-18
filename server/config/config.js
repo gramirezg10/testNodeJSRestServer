2cdc3944c4e4de867a64fe975a5961e927487005
German Ramirez <mancho@MacBook-Pro-de-German.local>
2019-12-17 22:16:14 -0500

    add comment to create an environment variable

--- server/config/config.js
+++ server/config/config.js
@@ -6,3 +6,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
 if(process.env.NODE_ENV === 'dev') process.env.URLDB = 'mongodb://localhost:27017/cafe'
 else process.env.URLDB = process.env.mongo_url
+
+// se ingresa el siguiente comando en la terminal para configurar la variable de ambiente
+// heroku config:set mongo_url="XXXXXXX"
