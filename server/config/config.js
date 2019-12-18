a2a6cf571f2f433cab7771cc12dc7c54492915d5
German Ramirez <mancho@MacBook-Pro-de-German.local>
2019-12-17 22:03:10 -0500

    variable de entorno heroku (DB)

--- server/config/config.js
+++ server/config/config.js
@@ -5,4 +5,4 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
 if(process.env.NODE_ENV === 'dev') process.env.URLDB = 'mongodb://localhost:27017/cafe'
-else process.env.URLDB = 'mongodb+srv://admin:admin@cluster0-4ynpz.mongodb.net/cafe'
+else process.env.URLDB = process.env.mongo_url
